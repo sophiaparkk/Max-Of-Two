0ad1d2e59755761cf897dc032b6fe67fb5b9fc30
Sophia Park <park.sophiakela@gmail.com>
2023-03-14 18:24:19 -0600

    Initial commit

---
+++ maxOfTwo.js
@@ -0,0 +1,12 @@
+let x = 1
+let y = 2
+
+// Write your code below this line.
+
+if (x > y) {
+    console.log(x)
+} else if (y > x) {
+    console.log(y)
+} else (x === y) {
+    console.log('The values are identical')
+}
